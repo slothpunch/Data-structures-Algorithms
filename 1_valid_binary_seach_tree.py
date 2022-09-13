86d94763578765b9cba070fd0dbd83ed7cc73a4a
Kim <giminsub1@gmail.com>
2022-09-13 22:08:29 +1000

    First Valid BST

---
+++ 1_valid_binary_seach_tree.py
@@ -0,0 +1,60 @@
+'''
+    1. Take nodes. Top, left, right
+    2. Find which one is greater and smaller than Top node. 
+    
+
+'''
+
+class Node():
+    # constructor
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        print(self.val, self.left, self.right)
+
+node = Node(5)
+node.right = Node(4)
+node.left = Node(6)
+
+# class Node(object):
+#   def __init__(self, val, left=None, right=None):
+#     self.val = val
+#     self.left = left
+#     self.right = right
+        
+# class Solution(object):
+#   def _isValidBSTHelper(self, n, low, high):
+#     if not n:
+#       return True
+#     val = n.val
+#     if ((val > low and val < high) and
+#         self._isValidBSTHelper(n.left, low, n.val) and
+#         self._isValidBSTHelper(n.right, n.val, high)):
+#         return True
+#     return False
+
+#   def isValidBST(self, n):
+#     return self._isValidBSTHelper(n, float('-inf'), float('inf'))
+
+
+# #   5
+# #  / \
+# # 4   7
+# node = Node(5)
+# node.left = Node(4)
+# node.right = Node(7)
+# print(Solution().isValidBST(node))
+
+# #   5
+# #  / \
+# # 4   7
+# #    /
+# #   2
+# node = Node(5)
+# node.left = Node(4)
+# node.right = Node(7)
+# node.right.left = Node(2)
+# print(Solution().isValidBST(node))
+# # False
